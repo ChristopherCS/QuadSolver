50e9dc4503774176208a76129a24b39e2c239f30
Chris Carlson <christopher.s.carlson@wmich.edu>
2018-10-18 14:41:12 -0400

    Created a MakeFile - functional. Crated quadsolver.h - functional (We should put all includes and function prototypes here.). Created mySquareRoot.c with function. Wrote the "solve" function in quadSolver.c which should solve the quadratic equation.

---
+++ SRC/mySqareRoot.c
@@ -0,0 +1,21 @@
+#include "quadSolver.h"
+
+// Computes the Square Root of input, and places that value into output. Returns 0 on success or 1 on failer.
+// When Called, mySquareRoot Logs that the square root function has been called. 
+int mySquareRoot(double input, double *output){
+	int ret = 0;
+	char *logMessage = (char *)calloc(sizeof(char), 256);
+
+	if(input < 0){
+		ret = 1;
+	}else{
+		*output = sqrt(input);
+
+		//Creates the Log Message with sprintf.
+		sprintf(logMessage, "Function \"mySquareRoot\" Called with input: %f, and output: %f", input, *output);
+		log(logMessage);
+	}
+
+	free(logMessage);
+	return(ret);
+}
---
+++ SRC/quadSolver.h
@@ -0,0 +1,11 @@
+#ifndef QUAD_SOLVER_HEADER
+#define QUAD_SOLVER_HEADER
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int mySquareRoot(double input, double *output); 
+int log(char *logMessage);
+int solve(float a, float b, float c);
+#endif
