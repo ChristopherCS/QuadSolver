058f7e5ddc0bff8d8ff0c362bf649f8a1e5530b8
Chris Carlson <christopher.s.carlson@wmich.edu>
2018-10-30 10:33:27 -0400

    Fixed the solve program again. Now we can be sure that the MOST NEGATIVE result will be the first result in results array. This will help with testing.

--- SRC/solver.c
+++ SRC/solver.c
@@ -14,10 +14,15 @@ int solve(float a, float b, float c, int *nRoots, float *result[2]){
 	double sqroot = 0.0;
 	char *logString = calloc(sizeof(char), 256);
 	double dmacheps = computeDMachEps(); // Used to calculate relative error between computed roots.
+	float temp; // Used to swap roots so that the most negative value is always the first value in the return array.
 
+
+	// If the result is non-real, we simply return and report that with the result value.
 	if(discriminant < 0){
 		logMessage("Result is non-real");
 		ret = 1;
+
+		// else we complete the calculation.
 	}else{
 		// If True, there was an error. Otherwise the
 		// Result of the square root function is in sqroot.
@@ -25,14 +30,24 @@ int solve(float a, float b, float c, int *nRoots, float *result[2]){
 			logMessage("Unknown Error Occurred.");
 			ret = 2;
 		}else{
-			*result[0] = (float)(-B + sqroot / 2*A);
-			*result[1] = (float)(-B - sqroot / 2*A);
+			*result[0] = (float)(-B - sqroot / 2*A);
+			*result[1] = (float)(-B + sqroot / 2*A);
+
+
 		//Test if the two roots are within rounding error of each other. If so, only one root. Otherwise two roots.
 		if(fabs(*result[0] - *result[1])/ (fabs(*result[0])+fabs(*result[1])) < 10*dmacheps){
+			*result[0] = *result[1];
 			*nRoots = 1;
 		}else *nRoots = 2;
 			sprintf(logString, "Roots are: %.8f, %.8f\n", (float)R1, (float)R2);
 			logMessage(logString);
+		}
+
+		// Now ensure that the most negative value is the first value in the array. 
+		if(*result[0] > *result[1]){
+			temp = *result[0];
+			*result[0] = *result[1];
+			*result[1] = temp;
 		}
 	}
 
