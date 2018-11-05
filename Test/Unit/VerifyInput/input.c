8459d325bf0a2e4d161595093f58bc1dd23ef33c
Chris Carlson <christopher.s.carlson@wmich.edu>
2018-11-05 14:53:18 -0500

    Minor Change

--- Test/Unit/VerifyInput/input.c
+++ Test/Unit/VerifyInput/input.c
@@ -2,33 +2,34 @@
 #include <string.h>
 #include "quadSolver.h"
 
-int input(int argc, char *argv[], char *result)
-{
-   
-  //  int argc_counter;
-  //  char values [256] = "";
-  //  int validation;
-  
-  //  if (argc == 4){
-   
-  //  	for (argc_counter = 1; argc_counter < argc;argc_counter++){ 
-  //  		if (strlen(values) + strlen(argv[argc_counter]) > 256){
-  //  			return 1;            //overflow error
-  //  		}else{
-	//    		strncat(values, argv[argc_counter], 80);		//80?! make it less, or whatever num it will be, it is still limited to the if condition
-	//    		strncat(values, " ", 1);
-	// 	}
-  // 	}	
+// to validate the values from the user input
+//int validate (char val[], double result[]);
 
-  //  	printf("%s\n",values);   	
-  //  }else if(argc == 1){
-  //   	printf("Enter the values for A, B, C:\n" );
-  //  		scanf("%[^\n]%*c",&values);  // or we can use fgets
-  //  		//printf("The values are: %s\n", values);
-  //  		//printf("%d\n",validation);
-  //   }
+int input(int argc, char *argv[], char *values) //char *result)
+{
+   int argc_counter;
    
-   return 0;
+   if (argc == 4){
+   
+    for (argc_counter = 1; argc_counter < argc;argc_counter++){ 
+      if (strlen(values) + strlen(argv[argc_counter]) > 256){
+        return 1;            //overflow error
+      }else{
+        strncat(values, argv[argc_counter], 80);    //80?! make it less, or whatever num it will be, it is still limited to the if condition
+        strncat(values, " ", 1);
+      }
+    } 
+    return 0;
+    //printf("The values are in input: %s\n", values); 
+   }else if(argc == 1){
+      printf("Enter the values for A, B, C:\n" );
+      scanf("%[^\n]%*c",values);  // or we can use fgets, difference between value , &value
+      //printf("The values are: %s\n", values);
+      return 0;
+    }//
+
+    printf("Wrong amount of arguments. Expecting 3 floating numbers.");
+   
+   return 1;
 
 }
-
