833c8993b111ca72a3365a65c04b2bf6bd200ace
Chris Carlson <christopher.s.carlson@wmich.edu>
2018-11-07 13:04:20 -0500

    Okay, everything appears to be working. Needs testing for the Solve function.

--- SRC/quadSolver.c
+++ SRC/quadSolver.c
@@ -5,8 +5,7 @@
 *		Oscar Vanderhorst
 *
 * Purpose:
-*		This program will return the X values of the quadratic equation 
-*		given a proper input of a, b & c.
+*		This program will return the X Values (Roots) of the quadratic equation given a proper input of a, b & c.
 *
 */
 
@@ -34,7 +33,7 @@ int main(int argc, char *argv[]){
 	 	errors("Entered invalid values. Please provide 3 valid floating numbers.\n");
 	//Calculate X's (If there haven't been any errors yet)
 
-	if((ret = solve(result[0], result[1], result[2], &nRoots,(double **) &roots))){
+	if((ret = solve(result[0], result[1], result[2], &nRoots,roots))){
 		//Error
 		switch(ret){
 			case 1:	
@@ -47,10 +46,10 @@ int main(int argc, char *argv[]){
 	else {
 		switch(nRoots){
 			case 1:
-				printf("There is 1 Root. It is %7f\n", roots[1]);
+			printf("\tX1: %8g\n\t*Only One Root\n", roots[1]);
 				break;
 			default:
-				printf("The Roots are:\n\t%f\n\t%f\n", roots[1], roots[2]);
+			printf("\tX1: %8g\n\tX2: %8g\n", roots[0], roots[1]);
 		}
 	}
 	//Exit program (Free pointers/Close files)
