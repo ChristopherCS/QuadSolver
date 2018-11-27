6ac386f0801c494d7dffba0953aa1836819f2408
Tyrna <mouthoftyrna@gmail.com>
2018-11-26 19:22:06 -0500

    added more tests

--- Test/Functional/FuncTest.sh
+++ Test/Functional/FuncTest.sh
@@ -6,16 +6,16 @@ inputPath='../Input/'
 
 input_files=(one.input two.input 
 	three.input four.input five.input six.input
-	seven.input)
+	seven.input eight.input nine.input)
 output_files=(one.output two.output 
 	three.output four.output five.output six.output
-	seven.output)
+	seven.output eight.output nine.output)
 
 #Run Make file to create executable
 #../../SRC/make
 
 
-for i in `seq 0 6`; do
+for i in `seq 0 8`; do
 #	echo "Loop #$i" 
 	inFile="${inputPath}${input_files[$i]}"
 	outFile="${inputPath}${output_files[$i]}"
